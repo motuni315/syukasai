dc918a5f0b0750c5d3898aac1147202ae634dc27
kurasakishoki <211020@std.hi-joho.ac.jp>
2024-06-14 17:00:06 +0900

    ハンバーがメニューの中身作り＆ulのレイアウト変更

---
+++ script.js
@@ -0,0 +1,18 @@
+// script.js
+
+function toggleMenu() {
+  var menuItems = document.getElementById("menuItems");
+  menuItems.classList.toggle("show-menu");
+  menuItems.classList.toggle("slide-in"); // スライドインのクラスを追加・削除
+}
+
+function closeMenuOutside() {
+  var menuItems = document.getElementById("menuItems");
+  menuItems.classList.remove("show-menu");
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  var nav = document.querySelector("nav");
+
+  nav.addEventListener("mouseleave", closeMenuOutside);
+});
