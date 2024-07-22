b95bd4fb020d7d42a7261684e6bc5681a34b7702
kurasakishoki <211020@std.hi-joho.ac.jp>
2024-07-22 23:25:59 +0900

    h1&h2の位置変更と画像の配置変更

--- script.js
+++ script.js
@@ -8,11 +8,7 @@ function toggleMenu() {
   menuItems.classList.toggle("show-menu");
   document.body.classList.toggle("show-close"); // メニューの表示・非表示に合わせて背景を調整
 
-  if (menuItems.classList.contains("show-menu")) {
-    document.body.style.overflow = "hidden"; // メニューが表示されたときにスクロールを禁止
-  } else {
-    document.body.style.overflow = ""; // メニューが非表示になったときにスクロールを許可
-  }
+
 }
 
 
