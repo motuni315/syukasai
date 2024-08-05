aa8417f3f78563332ece8303771adbd5a1734cd6
kurasakishoki <211020@std.hi-joho.ac.jp>
2024-08-05 16:02:29 +0900

    お問い合わせをメアドに変更

--- script.js
+++ script.js
@@ -1,20 +1,13 @@
-// script.js
+document.addEventListener('DOMContentLoaded', function() {
+  const emailElement = document.getElementById('email');
 
-function toggleMenu() {
-  var menuItems = document.getElementById("menuItems");
-  var closeIcon = document.querySelector(".close-icon");
-  var openBtn = document.querySelector(".openbtn");
-
-  menuItems.classList.toggle("show-menu");
-  document.body.classList.toggle("show-close"); // メニューの表示・非表示に合わせて背景を調整
-
-
-}
-
-
-function closeMenuOutside() {
-  var menuItems = document.getElementById("menuItems");
-  var closeIcon = document.querySelector(".close-icon");
-  menuItems.classList.remove("show-menu");
-  document.body.classList.remove("show-close"); // Hide the close icon
-}
+  emailElement.addEventListener('click', function() {
+    // メールアドレスをクリップボードにコピーする
+    const email = emailElement.textContent;
+    navigator.clipboard.writeText(email).then(function() {
+      alert('メールアドレスがコピーされました: ' + email);
+    }).catch(function(error) {
+      console.error('コピーに失敗しました: ', error);
+    });
+  });
+});
