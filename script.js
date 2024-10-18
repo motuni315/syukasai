b73da295094667666c3823f45f0808631976600b
kurasakishoki <211020@std.hi-joho.ac.jp>
2024-10-18 17:21:03 +0900

    ４Fフロアマップの位置調整

--- script.js
+++ script.js
@@ -1,24 +1,14 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const telElement = document.getElementById('tel');
+function highlightRoom(roomId) {
 
-  telElement.addEventListener('click', function() {
-    // 電話番号をクリップボードにコピーする
-    const tel = telElement.textContent;
-    navigator.clipboard.writeText(tel).then(function() {
-      alert('電話番号がコピーされました: ' + tel);
-    }).catch(function(error) {
-      console.error('コピーに失敗しました: ', error);
-    });
+  event.preventDefault();
+  // すでにハイライトされている教室を解除
+  document.querySelectorAll('.highlight').forEach(area => {
+    area.classList.remove('highlight');
   });
 
-  const links = document.querySelectorAll('a[href^="#"]');
-  for (const link of links) {
-    link.addEventListener("click", function(e) {
-      e.preventDefault();
-      const targetId = link.getAttribute("href");
-      document.querySelector(targetId).scrollIntoView({
-        behavior: "smooth"
-      });
-    });
+  // 指定された教室をハイライト
+  const room = document.getElementById(roomId);
+  if (room) {
+    room.classList.add('highlight');
   }
-});
+}
